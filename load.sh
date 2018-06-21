a0b0432051d1188851f4015379e785ff0d5c2f87
Alexey Rybalchenko <alexryba@gmail.com>
2018-06-21 16:59:48 +0200

    add load/unload scripts for reference

---
+++ load.sh
@@ -0,0 +1,3 @@
+export ALIBUILD_WORK_DIR=/home/arybalch/dev/alice/sw
+eval `/home/arybalch/.local/bin/alienv --no-refresh load O2/latest`
+
---
+++ unload.sh
@@ -0,0 +1,3 @@
+export ALIBUILD_WORK_DIR=/home/arybalch/dev/alice/sw
+eval `/home/arybalch/.local/bin/alienv --no-refresh unload O2/latest`
+
